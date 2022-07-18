b769ef79642f0e5a73a7bd977d9802e618956ce1
Radomir Stevanovic <radomir@dwavesys.com>
2022-07-18 08:02:18 -0700

    Allow for sub-tag explicit aliases

--- generate-tags.py
+++ generate-tags.py
@@ -28,37 +28,40 @@ OCEAN_VERSION = '5.3.0'
 # ocean version under build, as named tuple
 ocean_version_info = namedtuple('Version', 'major minor patch')(*OCEAN_VERSION.split('.'))
 
-def get_version(version_info, significant_parts=None):
+def _get_version(version_info, significant_parts=None):
     if significant_parts is None:
         significant_parts = len(version_info)
     return '.'.join(version_info[:significant_parts])
 
-# build a cartesian product of these subcomponents (subtags)
-# (`None` = use a default value as defined in `DEFAULTS`)
-OCEAN_VERSIONS = {
-    None}.union(get_version(ocean_version_info, n+1) for n in range(len(ocean_version_info)))
-PYTHON_VERSIONS = {
-    None, 'python3.8', 'python3.9', 'python3.10'}
-PLATFORM_NAMES = {
-    None, 'bullseye', 'slim'}
+OCEAN_VERSIONS_ROUNDED = [_get_version(ocean_version_info, n+1)
+                          for n in range(len(ocean_version_info))]
 
-DEFAULTS = dict(
-    platform='bullseye', ocean=OCEAN_VERSION, python='python3.9')
+assert len(OCEAN_VERSIONS_ROUNDED) == 3
+OCEAN_VERSION_MAJOR, OCEAN_VERSION_MINOR, OCEAN_VERSION_PATCH = OCEAN_VERSIONS_ROUNDED
 
+# build a cartesian product of these subcomponents (sub-tags)
+# (`None` = use a value as defined in `CANONICAL_TAGS`, but leave out the sub-tag name)
+OCEAN_VERSIONS = {None}.union(OCEAN_VERSIONS_ROUNDED)
+PYTHON_VERSIONS = {None, 'python3.8', 'python3.9', 'python3.10'}
+PLATFORM_TAGS = {None, 'bullseye', 'slim', 'slim-bullseye'}
 
-def get_canonical_subtag(name, value):
-    """Return canonical tag value for component `name`."""
-
-    if value is None:
-        return DEFAULTS[name]
-
-    if name == 'ocean':
-        if OCEAN_VERSION.startswith(value):
-            return OCEAN_VERSION
-        # TODO: to resolve any ocean version to canonical (full spec), we need all versions released
-        raise ValueError("Unable to resolve Ocean version other than one under build.")
-
-    return value
+# sub-tag alias map
+# note: expanded here for clarity (vs scripted)
+CANONICAL_TAGS = {
+    'ocean': {
+        None: OCEAN_VERSION,
+        OCEAN_VERSION_MAJOR: OCEAN_VERSION,
+        OCEAN_VERSION_MINOR: OCEAN_VERSION,
+        OCEAN_VERSION_PATCH: OCEAN_VERSION,
+    },
+    'python': {
+        None: 'python3.9'
+    },
+    'platform': {
+        None: 'bullseye',
+        'slim': 'slim-bullseye',
+    }
+}
 
 
 def get_tag(default_tag='latest', **subtags):
@@ -69,8 +72,8 @@ def get_tag(default_tag='latest', **subtags):
     else:
         tag = default_tag
 
-    # derive canonical tag (`tag` can be an alias)
-    canonical_subtags = (get_canonical_subtag(k, v) for k, v in subtags.items())
+    # derive canonical sub-tag (from, possibly, an alias)
+    canonical_subtags = (CANONICAL_TAGS[k].get(v, v) for k, v in subtags.items())
     canonical_tag = '-'.join(canonical_subtags)
 
     return tag, canonical_tag
@@ -78,7 +81,7 @@ def get_tag(default_tag='latest', **subtags):
 
 tagbag = defaultdict(set)   # Dict[str, Set[str]]
 
-for oc,py,pl in product(OCEAN_VERSIONS, PYTHON_VERSIONS, PLATFORM_NAMES):
+for oc,py,pl in product(OCEAN_VERSIONS, PYTHON_VERSIONS, PLATFORM_TAGS):
     tag, canonical = get_tag(ocean=oc, python=py, platform=pl)
     tagbag[canonical].add(tag)
 
@@ -87,7 +90,7 @@ all_tags = set(tagbag.keys()).union(*tagbag.values())
 print(f"===\nmatrix\n===\n"
       f"- ocean: {', '.join(filter(None, OCEAN_VERSIONS))}\n"
       f"- python: {', '.join(filter(None, PYTHON_VERSIONS))}\n"
-      f"- platform: {', '.join(filter(None, PLATFORM_NAMES))}\n")
+      f"- platform: {', '.join(filter(None, PLATFORM_TAGS))}\n")
 
 print("===\ncanonical images/tags:", len(tagbag), '\n===')
 for canonical, aliases in tagbag.items():
