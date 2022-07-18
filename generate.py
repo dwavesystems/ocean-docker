40586c36760b496e77bab8afb4c6972bbfbbe9bf
Radomir Stevanovic <radomir@dwavesys.com>
2022-07-18 09:45:09 -0700

    Refactor tag generator script as CLI tool

--- generate.py
+++ generate.py
@@ -17,13 +17,17 @@ Generate/preview all tags a build matrix should generate, together with each
 tag mapped to a canonical tag.
 """
 
+import os
 from itertools import product
 from collections import defaultdict, namedtuple
+
+import click
+import jinja2
 
 
 # ocean version under build
 # TODO: get latest from github releases
-OCEAN_VERSION = '5.3.0'
+OCEAN_VERSION = os.environ['OCEAN_VERSION']
 
 # ocean version under build, as named tuple
 ocean_version_info = namedtuple('Version', 'major minor patch')(*OCEAN_VERSION.split('.'))
@@ -64,7 +68,8 @@ CANONICAL_TAGS = {
 }
 
 
-def get_tag(default_tag='latest', **subtags):
+def build_tag(default_tag='latest', **subtags):
+    """Build a tag from sub-tags. Return (tag, canonical tag) tuple."""
     # `subtags` key order matters! use: (ocean, python, platform)
     nonempty_subtags = list(filter(None, subtags.values()))
     if nonempty_subtags:
@@ -79,23 +84,45 @@ def get_tag(default_tag='latest', **subtags):
     return tag, canonical_tag
 
 
-tagbag = defaultdict(set)   # Dict[str, Set[str]]
+def get_tags(ocean_versions, python_versions, platform_tags):
+    """Produce a map of canonical tags (that we need to build) to a bag of
+    alias tags.
+    """
+    tag_bags = defaultdict(set)   # Dict[str, Set[str]]
 
-for oc,py,pl in product(OCEAN_VERSIONS, PYTHON_VERSIONS, PLATFORM_TAGS):
-    tag, canonical = get_tag(ocean=oc, python=py, platform=pl)
-    tagbag[canonical].add(tag)
+    for oc, py, pl in product(ocean_versions, python_versions, platform_tags):
+        tag, canonical_tag = build_tag(ocean=oc, python=py, platform=pl)
+        tag_bags[canonical_tag].add(tag)
 
-all_tags = set(tagbag.keys()).union(*tagbag.values())
+    return tag_bags
 
-print(f"===\nmatrix\n===\n"
-      f"- ocean: {', '.join(filter(None, OCEAN_VERSIONS))}\n"
-      f"- python: {', '.join(filter(None, PYTHON_VERSIONS))}\n"
-      f"- platform: {', '.join(filter(None, PLATFORM_TAGS))}\n")
 
-print("===\ncanonical images/tags:", len(tagbag), '\n===')
-for canonical, aliases in tagbag.items():
-    print(f'{canonical}:\n  {", ".join(sorted(aliases.difference(canonical)))}\n')
+@click.group()
+def cli():
+    """Generate tags or dockerfiles."""
 
-print(f"===\nall tags: {len(all_tags)}\n===")
-for tag in sorted(all_tags):
-    print(tag)
+@cli.command()
+def tags():
+    """Print tags to build for OCEAN_VERSION from environment."""
+    tag_bags = get_tags(OCEAN_VERSIONS, PYTHON_VERSIONS, PLATFORM_TAGS)
+    all_tags = set(tag_bags.keys()).union(*tag_bags.values())
+
+    print(f"===\nmatrix\n===\n"
+        f"- ocean: {', '.join(filter(None, OCEAN_VERSIONS))}\n"
+        f"- python: {', '.join(filter(None, PYTHON_VERSIONS))}\n"
+        f"- platform: {', '.join(filter(None, PLATFORM_TAGS))}\n")
+
+    print("===\ncanonical images/tags:", len(tag_bags), '\n===')
+    for canonical, aliases in tag_bags.items():
+        print(f'{canonical}:\n  {", ".join(sorted(aliases.difference(canonical)))}\n')
+
+    print(f"===\nall tags: {len(all_tags)}\n===")
+    for tag in sorted(all_tags):
+        print(tag)
+
+@cli.command()
+def dockerfiles():
+    pass
+
+if __name__ == '__main__':
+    cli()
